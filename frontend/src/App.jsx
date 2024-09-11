ae001111a32276049e865e3026d8a97ab7592c47
AnuragM23 <anuragmahato006@gmail.com>
2024-09-11 14:50:30 +0530

    completed the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import About from './pages/About'
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path='/my-profile' element={<MyProfile/>}/>
         <Route path='/my-appointments' element={<MyAppointments/>}/>
         <Route path='/appointment/:docId' element={<Appointment/>}/>
+        <Route path='/about' element={<About />}/>
       </Routes>
     </div>
   )
