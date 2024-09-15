aededb09db8dfaf5311802c76ced961bfff03b20
AnuragM23 <anuragmahato006@gmail.com>
2024-09-15 13:31:37 +0530

    Creating the appointment booking page

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -4,16 +4,16 @@ import { doctors } from "../assets/assets";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  const currencySymbol = "$";
 
-    const value = {
-        doctors
-    }
+  const value = {
+    doctors,
+    currencySymbol,
+  };
 
-    return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    );
-}
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
 
 export default AppContextProvider;
