83b32eea95cef2312f70e6f724d6c6c7ed6cf63a
AnuragM23 <anuragmahato006@gmail.com>
2024-09-17 13:58:58 +0530

    working on the time slots component of the appointment page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -6,39 +6,40 @@ import { assets } from "../assets/assets";
 function Appointment() {
   const { docId } = useParams();
   const { doctors, currencySymbol } = useContext(AppContext);
-  const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
   const [docInfo, setDocInfo] = useState({});
   const [docSlots, setDocSlots] = useState([]);
   const [slotIndex, setSlotIndex] = useState(0);
-  const [slotTime, setSlotTime] = useState('');
-
+  const [slotTime, setSlotTime] = useState("");
 
   const fetchDocInfo = async () => {
-    const docInfoSearch = await doctors.find((doc) => doc._id === docId);
+    const docInfoSearch = doctors.find((doc) => doc._id === docId);
     setDocInfo(docInfoSearch);
-    // console.log(docInfoSearch);
+    
   };
 
   const getAvialableSlots = async () => {
     setDocSlots([]);
-    
+
     // getting current date
     let today = new Date();
-    for(let i=0; i<7; i++) {
+    for (let i = 0; i < 7; i++) {
       // getting date with index
       let currentDate = new Date(today);
-      currentDate.setDate(today.getDate()+i);
+      currentDate.setDate(today.getDate() + i);
 
       // setting end time of the date with index
       let endTime = new Date();
-      endTime.setDate(today.getDate()+1);
-      endTime.setHours(21,0,0,0);
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
 
       // setting hours
-      if(today.getDate() === currentDate.getDate()) {
-        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours():10);
-        currentDate.setMinutes(currentDate.getMinutes()>30?30:0)
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(
+          currentDate.getHours() > 10 ? currentDate.getHours() : 10
+        );
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
       } else {
         currentDate.setHours(10);
         currentDate.setMinutes(0);
@@ -46,22 +47,25 @@ function Appointment() {
 
       let timeSlots = [];
 
-      while(currentDate < endTime) {
-        let formattedTime = currentDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
 
         // add slot to array
         timeSlots.push({
           datetime: new Date(currentDate),
-          time: formattedTime
+          time: formattedTime,
         });
 
         // increment current time by 30 minutes
-        currentDate.setMinutes(currentDate.getMinutes()+30);
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
       }
 
-      setDocSlots(prev => ([...prev, timeSlots]));
+      setDocSlots((prev) => [...prev, timeSlots]);
     }
-  }
+  };
 
   useEffect(() => {
     fetchDocInfo();
@@ -69,10 +73,14 @@ function Appointment() {
 
   useEffect(() => {
     getAvialableSlots();
-  },[docInfo]);
+  }, [docInfo]);
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(docSlots);
+    console.log(typeof docSlots);
+    console.log(docSlots[0].datetime.getDay());
+    
+    
   }, [docSlots]);
 
   return (
@@ -80,10 +88,14 @@ function Appointment() {
       {/* Doctor Details */}
       <div className="flex flex-col sm:flex-row gap-4 ">
         <div>
-          <img className="bg-primary w-full sm:max-w-72 rounded-lg" src={docInfo.image} alt="" />
+          <img
+            className="bg-primary w-full sm:max-w-72 rounded-lg"
+            src={docInfo.image}
+            alt=""
+          />
         </div>
 
-        <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0" >
+        <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
           {/* Doc Info : name, degree, experience */}
           <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
             {docInfo.name}
@@ -93,7 +105,9 @@ function Appointment() {
             <p>
               {docInfo.degree} - {docInfo.speciality}
             </p>
-            <button className="py-0.5 px-2 border text-xs rounded-full">{docInfo.experience}</button>
+            <button className="py-0.5 px-2 border text-xs rounded-full">
+              {docInfo.experience}
+            </button>
           </div>
 
           {/* Doctor About */}
@@ -101,11 +115,30 @@ function Appointment() {
             <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
               About <img src={assets.info_icon} alt="" />
             </p>
-            <p className="text-sm text-gray-500 max-w-[700ox] mt-1">{docInfo.about}</p>
+            <p className="text-sm text-gray-500 max-w-[700ox] mt-1">
+              {docInfo.about}
+            </p>
           </div>
           <p className="text-gray-500 font-medium mt-4">
-            Appointment fee : <span className="text-gray-600">{currencySymbol} {docInfo.fees}</span>
+            Appointment fee :{" "}
+            <span className="text-gray-600">
+              {currencySymbol} {docInfo.fees}
+            </span>
           </p>
+        </div>
+      </div>
+
+      {/* Booking Slots */}
+      <div className="sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700">
+        <p>Booking slots</p>
+        <div>
+          {docSlots.length &&
+            docSlots.map((item, index) => {
+              <div key={index}>
+                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>;
+            })}
         </div>
       </div>
     </div>
