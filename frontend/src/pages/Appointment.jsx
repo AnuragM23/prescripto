d2095136823ca2522c6715173e0bc02a9c986ff3
AnuragM23 <anuragmahato006@gmail.com>
2024-09-21 12:38:33 +0530

    created the about, contact and login page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -131,7 +131,7 @@ function Appointment() {
         <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
           {
             docSlots.length && docSlots.map((item, index) => (
-              <div onClick={()=>setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${(slotIndex === index)?'bg-primary text-white':'border border-gray-200'}`} key={index}>
+              <div onClick={()=>setSlotIndex(index)} className={`mb-4 text-center py-6 min-w-16 rounded-full cursor-pointer ${(slotIndex === index)?'bg-primary text-white':'border border-gray-200'}`} key={index}>
                 <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
                 <p>{item[0] && item[0].datetime.getDate()}</p>
               </div>
